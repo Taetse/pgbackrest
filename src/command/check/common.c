46f38bfd46f55b01994148009cb005209e062300
taetse <taetse.durand@gmail.com>
2025-05-01 05:04:17 +0200

    remove validation

--- src/command/check/common.c
+++ src/command/check/common.c
@@ -59,16 +59,6 @@ checkDbConfig(const unsigned int pgVersion, const unsigned int pgIdx, const Db *
         const unsigned int dbVersion = dbPgVersion(dbObject);
         const String *const dbPath = dbPgDataPath(dbObject);
 
-        if (pgVersion != dbVersion || strCmp(cfgOptionIdxStr(cfgOptPgPath, pgIdx), dbPath) != 0)
-        {
-            THROW_FMT(
-                DbMismatchError, "version '%s' and path '%s' queried from cluster do not match version '%s' and '%s' read from '%s/"
-                PG_PATH_GLOBAL "/" PG_FILE_PGCONTROL "'\nHINT: the %s and %s settings likely reference different clusters.",
-                strZ(pgVersionToStr(dbVersion)), strZ(dbPath), strZ(pgVersionToStr(pgVersion)),
-                strZ(cfgOptionIdxDisplay(cfgOptPgPath, pgIdx)), strZ(cfgOptionIdxDisplay(cfgOptPgPath, pgIdx)),
-                cfgOptionIdxName(cfgOptPgPath, pgIdx), cfgOptionIdxName(cfgOptPgPort, pgIdx));
-        }
-
         // Check archive configuration if option is valid for the command and set
         if (!isStandby && cfgOptionValid(cfgOptArchiveCheck) && cfgOptionBool(cfgOptArchiveCheck))
         {
